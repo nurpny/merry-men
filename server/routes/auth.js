0c24caddcf86d751c9b955ad1ba9a41a6a7f4cd1
nurpny <runnurrr@gmail.com>
2020-05-20 20:46:36 -0400

    Add auth routes

--- server/routes/auth.js
+++ server/routes/auth.js
@@ -1,3 +1,32 @@
 const router = require('express').Router()
 
+router.post('/login', async (req, res, next) => {
+  try {
+    const user = await User.verifyPassword(req.body.email, req.body.password)
+    if (!user) {
+      res.status(401).send('Wrong username and/or password')
+    } else {
+      req.login(user, err => (err ? next(err) : res.json(user)))
+    }
+  } catch (err) {
+    next(err)
+  }
+})
+
+
+router.post('/signup', async (req, res, next) => {
+  try {
+    const user = await User.create(req.body)
+    req.login(user, err => (err ? next(err) : res.json(user)))
+  } catch (err) {
+    if (err.name === 'SequelizeUniqueConstraintError') {
+      res.status(401).send('User already exists')
+    } else {
+      next(err)
+    }
+  }
+})
+
+
+
 module.exports = router
