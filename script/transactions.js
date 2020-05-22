928709a62b53f7e1c0c12f5b6b8239af34bf3a87
nurpny <runnurrr@gmail.com>
2020-05-21 21:21:19 -0400

    Update seed file

--- script/transactions.js
+++ script/transactions.js
@@ -10,19 +10,19 @@ module.exports = [
     symbol: "AAPL",
     quantity: -10,
     price: 31496,
-    createdAt: new Date("2015-05-18T16:00:00Z")
+    createdAt: new Date("2020-05-18T16:00:00Z")
   }, {
     userId: 1,
     symbol: "NFLX",
     quantity: 10,
     price: 43363,
-    createdAt: new Date("2015-05-14T15:00:00Z")
+    createdAt: new Date("2020-05-14T15:00:00Z")
   }, {
     userId: 1,
     symbol: "NFLX",
     quantity: -5,
     price: 45230,
-    createdAt: new Date("2015-05-19T16:00:00Z")
+    createdAt: new Date("2020-05-19T16:00:00Z")
   }, {
     userId: 2,
     symbol: "SPOT",
